ac4dcdad6b250caf8e659c30a1857d02ec44d25c
Heather Ingargiola <h.ingargiola88@gmail.com>
2021-02-09 15:42:10 -0600

    Pizza Place Homework

---
+++ week2.js
@@ -0,0 +1,8 @@
+const pizzaPlace = "Ingargiola's";
+let numberOfToppings = 26;
+console.log(pizzaPlace);
+console.log(numberOfToppings);
+console.log(`"${pizzaPlace} is the best pizza place around!"`);
+if (numberOfToppings < 10) {
+  console.log("Quality, not quantity.");
+}
