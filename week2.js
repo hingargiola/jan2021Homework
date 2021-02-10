0003b505eae81bc68c9426a27bde572b5f32d68a
Heather Ingargiola <h.ingargiola88@gmail.com>
2021-02-09 18:22:04 -0600

    update pizza

--- week2.js
+++ week2.js
@@ -5,4 +5,6 @@ console.log(numberOfToppings);
 console.log(`"${pizzaPlace} is the best pizza place around!"`);
 if (numberOfToppings < 10) {
   console.log("Quality, not quantity.");
+} else {
+  console.log("A whole lot of pizza");
 }
